dfd6e25da43e418da40287cd878f455b6f9dae91
ZeccaLehn <zgit@outlook.com>
2023-05-18 13:47:34 -0700

    Initial test website

---
+++ script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Fetch the CSV file
+  fetch('data.csv')
+    .then(response => response.text())
+    .then(data => {
+      // Parse CSV data
+      const rows = data.split('\n');
+      var headers = rows[0].split(',');
+      const projects = [];
+      headers = headers.map(e => e.trimEnd());
+      console.log(headers);
+      
+
+      // Create an object for each project
+      for (let i = 1; i < rows.length; i++) {
+        const values = rows[i].split(',');
+
+        // Skip empty lines
+        if (values.length === 1 && !values[0]) {
+          continue;
+        }
+
+        const project = {};
+
+        for (let j = 0; j < headers.length; j++) {
+          if (headers[j] === 'sdgs') {
+            // Remove quotes from SDG value
+            project[headers[j]] = values[j].replace(/'/g, '');
+          } else {
+            project[headers[j]] = values[j].trim(); // Trim the values
+          }
+        }
+
+        projects.push(project);
+      }
+
+      // Generate HTML content for each project
+      const projectContainer = document.getElementById('project-container');
+      let html = '';
+
+      for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+
+        // Retrieve SDGs value and split into array
+        // const sdgs = project.sdgs ? project.sdgs.split('/') : [];
+
+        // Generate project card HTML
+        const projectCard = `
+          <div class="card">
+            <a target="_blank" href="${project.url}">
+              <img class="demo" src="images/${project.image}" alt="${project.name}">
+            </a>
+            <div>
+              <h3>
+                <a target="_blank" href="${project.url}">${project.name}</a>
+              </h3>
+              <p class="card-content">${project.content}</p>
+              <div class="tags">
+                <span class="tag">${project.stage}</span>
+                <span class="tag">${project.year}</span>
+                <span class="tag">${project.role}</span>
+                <span class="tag">SDGs: ${project.sdgs}</span>
+               
+              </div>
+            </div>
+          </div>
+        `;
+
+        html += projectCard;
+      }
+
+      // Insert generated HTML into the project container
+      projectContainer.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
